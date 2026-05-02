e3944cc907953235b8a24493884283d92f66f5cd
Piyush Gore <gp551011@gmail.com>
2026-05-02 13:01:41 +0530

    Add Base and Derived classes for inheritance example

---
+++ Fundamentals/Java/04_Inheritance/Single.java
@@ -0,0 +1,24 @@
+class Base
+{
+  public int i,j;
+  public Base()
+  {
+    
+  }
+  public void fun()
+  {
+
+  }
+}
+class Derived extends Base
+{
+    public int x,y;
+    public Derived()
+    {
+    
+    }
+    public void gun()
+    {
+
+    }
+}
