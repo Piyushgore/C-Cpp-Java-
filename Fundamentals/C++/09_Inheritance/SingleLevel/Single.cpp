a802321a409c59916a3f4d2cc7b56ca62ee80234
Piyush Gore <gp551011@gmail.com>
2026-05-02 12:55:49 +0530

    Implement single level inheritance with Base and Derived classes

---
+++ Fundamentals/C++/09_Inheritance/SingleLevel/Single.cpp
@@ -0,0 +1,34 @@
+class Base
+{
+  public:
+    int i,j;
+    Base()
+    {
+    
+    }
+    ~Base()
+    {
+    
+    }
+    void fun()
+    {
+    
+    }
+};
+class Derived:public Base
+{
+  public:
+    int x,y;
+    Derived()
+    {
+    
+    }
+    ~Derived()
+    {
+    
+    }
+    void gun()
+    {
+
+    }
+};
