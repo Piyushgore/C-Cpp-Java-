e74b596661242168a515e1ea751d2b3122b62677
Piyush Gore <gp551011@gmail.com>
2026-05-05 13:05:21 +0530

    Add files via upload

---
+++ Fundamentals/C++/07_Overloading/Redefinition.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int i,j;
+
+        void fun()
+        { cout<<"Inside Base fun\n";   }
+
+        void gun()
+        { cout<<"Inside Base gun\n";   }
+        void sun()
+        { cout<<"Inside Base sun\n";   }
+
+};      //8 Byte
+
+class Derived : public Base
+{
+    public:
+        int x,y;
+        void fun()      //Redefinition
+        { cout<<"Inside Derived fun\n";   }
+        void sun()
+        { cout<<"Inside Derived sun\n";   }
+        void run()      //Definition
+        { cout<<"Inside Derived run\n";   }
+
+};      //16 Byte
+
+int main()
+{
+    Base *bp = NULL;
+
+    bp = new Derived();     //upcasting
+
+    bp->fun();      //Base fun will be called
+    bp->gun();      //Base gun --||--
+    bp->sun();      //Base sun --||--
+    // bp->run();      //Error
+    return 0;
+}
