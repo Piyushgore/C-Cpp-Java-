1e2dd13aa1ba9e13a09a1f1702e9b979449d0073
Piyush Gore <gp551011@gmail.com>
2026-05-06 13:15:38 +0530

    Add UserdefinedNamespaceX.cpp with Marvellous namespace

---
+++ Fundamentals/C++/15_Namespace/UserdefinedNamespaceX.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+namespace Marvellous
+{
+    int no = 11;
+    void fun()
+    {
+        std::cout<<"Marvellous\n";
+    }
+}
+
+using namespace Marvellous;
+
+int main()
+{
+    std::cout<<"Jay Ganesh...";
+    fun();
+    return 0;
+}
